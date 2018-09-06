6a191b4e69c993e3af8b85a1783d76c7521d0137
petrov <piotr.wydrzycki@yahoo.com>
2018-09-06 07:23:05 -0400

    add user model

--- kdm/config/routes.rb
+++ kdm/config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
+	root 'welcome#index' 
   get 'welcome/index'
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/delete'
+	resources :users, only: [:new, :create, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
