aec271e7e79a4c741b2b28ab2c93e9c1e943a3b7
petrov <piotr.wydrzycki@yahoo.com>
2018-09-06 15:14:29 -0400

    create session and user views

---
+++ kdm/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+	def create
+		user = User.find_by email: params[:email]
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			flash[:notice] = 'You are signed in'
+		  redirect_to root_url
+		else
+			flash.now[:alert] = 'Invalid email/password'
+			render new
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = 'You are now singed out'
+		redirect_to root_url
+	end
+end
--- kdm/config/routes.rb
+++ kdm/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	root 'welcome#index' 
   get 'welcome/index'
-
+	resources :sessions, only: [:create]
+	get '/login', to: 'sessions#new'
+	get '/logout', to: 'sessions#destroy'
 	resources :users, only: [:new, :create, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
