f05b877b40ade896444c140a4f88ee8456c0405b
petrov <piotr.wydrzycki@yahoo.com>
2018-09-10 07:33:31 -0400

    generate order_item model

--- kdm/db/schema.rb
+++ kdm/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910112230) do
+ActiveRecord::Schema.define(version: 20180910113220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
@@ -37,5 +46,7 @@ ActiveRecord::Schema.define(version: 20180910112230) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
 end
--- kdm/spec/models/order_item_spec.rb
+++ kdm/spec/models/order_item_spec.rb
@@ -1,29 +1,5 @@
-describe OrderItem, :type => :model do
-	subject { described_class.new(order_id: "1", product_id: "1", quantity: "1")
-	}
-	describe 'Validations' do
-		it "is valid with attributes" do
-			expect(subject).to be_valid
-		end
-		
-		it 'is not valid without order_id' do
-			subject.order_id = nil
-			expect(subject).to be_invalid
-		end
+require 'rails_helper'
 
-		it 'is not valid without product_id' do
-			subject.product_id = nil
-			expect(subject).to be_invalid
-		end		
-
-		it 'is not valid with quantity is less that 0' do 
-			subject.quantity = "-1"
-			expect(subject).to be_invalid
-		end
-
-		it 'is valid with quantity = 0' do
-			subject.quantity = '0'
-			expect(subject).to be_valid
-		end
-	end
+RSpec.describe OrderItem, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
